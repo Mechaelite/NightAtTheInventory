d848c0a379bd75cd2c82e095a1cf2ebe8cf83225
joseluis1412 <pepelu1412@gmail.com>
2018-01-09 20:54:04 +0100

    Add files via upload

---
+++ pruebas css/index.php
@@ -0,0 +1,38 @@
+<?php # Script 3.4 - index.php
+$page_title = 'Welcome to this Site!';
+include ('./includes/header.html');
+?>
+
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="Style.css" type="text/css" media="screen">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js%22%3E"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js%22%3E"></script>
+</head>
+<body>
+
+
+<div class="topnav">
+
+  <a href="#">Logout</a>
+</div>
+<div class="container">
+        <ul>
+            <li><a class="active" href="#home">Weapons</a></li>
+            <li><a href="#news">Armor</a></li>
+            <li><a href="#contact">Misc</a></li>
+            <li><a href="#about">Characters</a></li>
+        </ul>
+
+<div class="column">
+    <h2>Column</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+</div>
+</div>
+
+<?php
+include ('./includes/footer.html');
+?>
