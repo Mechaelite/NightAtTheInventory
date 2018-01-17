cefd93bc00dcc49128f2665fb58392b1e7c279f9
Niten Ichi-Ryû <pau.colom.fp@gmail.com>
2018-01-17 17:24:44 +0100

    show all games / register game / sql files

---
+++ game/game.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Game(
+id_game mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+name_game varchar(40) NOT NULL,
+pj_game varchar(40) NOT NULL,
+gm_game varchar(40) NOT NULL,
+PRIMARY KEY (id_game)
+)
